01d0f797017c86e91d0955f284016bdcd3a512f0
web-pers <enmds13@gmail.com>
2025-03-02 16:02:37 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("Navigation Loaded!");
-});
+function toggleMenu() {
+    const menu = document.getElementById("side-menu");
+    if (menu.style.left === "0px") {
+        menu.style.left = "-300px"; // 닫기
+    } else {
+        menu.style.left = "0px"; // 열기
+    }
+}
